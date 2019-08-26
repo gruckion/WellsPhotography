efffa6c9433d5020f491b79a566943f2b43a3534
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-08-26 16:21:54 +0100

    Updates

---
+++ precache-manifest.91e9902153f6011f202a9e97dc50895a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d94ba2169c5b4978ca8",
+    "url": "/WellsPhotography/static/css/main.48d1d9d1.chunk.css"
+  },
+  {
+    "revision": "2d94ba2169c5b4978ca8",
+    "url": "/WellsPhotography/static/js/main.fb9ce486.chunk.js"
+  },
+  {
+    "revision": "429a6384e78d9c8d4ac8",
+    "url": "/WellsPhotography/static/js/runtime~main.68ce3c64.js"
+  },
+  {
+    "revision": "14c3ed02136a993f944a",
+    "url": "/WellsPhotography/static/js/2.282153f2.chunk.js"
+  },
+  {
+    "revision": "eac533ae63d33653787e2edf0ba0a1c3",
+    "url": "/WellsPhotography/static/media/facebook.eac533ae.webp"
+  },
+  {
+    "revision": "207e6cd2504fa9843b423250cce31b31",
+    "url": "/WellsPhotography/static/media/instagram.207e6cd2.webp"
+  },
+  {
+    "revision": "9aaa80478d06fddc24e1d612af4a7a6f",
+    "url": "/WellsPhotography/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a54d0dd8a1bcb548030a17cb40fe9b63.js"
+  "/WellsPhotography/precache-manifest.91e9902153f6011f202a9e97dc50895a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/WellsPhotography/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
